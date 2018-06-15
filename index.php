f0f6e5341862b1b2c2efb78fe52886c14670715b
everydaylearning123 <38413524+everydaylearning123@users.noreply.github.com>
2018-06-15 10:18:18 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" type="text/css" href="themes/bars-1to10.css">
+
+	<script type="text/javascript" src="jquery-1.11.2.min.js"></script>
+	<script type="text/javascript" src="jquery.barrating.min.js"></script>
+	<script type="text/javascript" src="examples.js"></script>
+
+
+	<style>
+		
+.wrapper
+{
+	background-image:url("rate.jpg");
+
+height: 100%; 
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+body, html {
+    height: 100%;
+}
+	</style>
+</head>
+<body>
+	<div class="wrapper">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4"></div>
+			<div class="col-lg-4">
+
+</div>
+
+<div class="col-lg-4" style="margin-top: 80px;"> <h2><b>Rating Experience</b></h2>
+  <div class="panel panel-default">
+    <div class="panel-heading">How Is Our Services ?</div>
+    <div class="panel-body"><div id="rating">
+	 <!-- select id need to remember -->
+	
+
+	<select id="example-1to10" name="rating" autocomplete="off">
+                 
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7" selected="selected">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+
+</div></div>
+    <div class="panel-footer"><button type="button" class="btn btn-primary">Submit</button></div>
+  </div>
+
+
+
+
+</div>
+
+
+</div>
+</div>
+
+</div> <!-- end of  wraper -->
+</body>
+</html>
